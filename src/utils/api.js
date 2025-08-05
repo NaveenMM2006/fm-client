05e3174d9b176efc75ffe7d8dc52bbeaffcb22c3
Naveen Mastamardi <mastamardinaveen524@gmail.com>
2025-08-05 19:09:50 +0530

    API URL  typo

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL:
-    'http://localhhttps://fm-server-2krr.onrender.comost:5000/api',
+    'https://fm-server-2krr.onrender.com',
 });
 
 
